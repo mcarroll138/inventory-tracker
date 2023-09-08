4d31668f3f8e4f8cab74d552ba5795da04bc0cee
Michael Carroll <mcarroll138@gmail.com>
2023-09-08 09:26:05 -0700

    create header component

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function header(){
+    return (
+        <h1>Lantern Rouge Inventory Control System</h1>
+    );
+}
+
+export default header;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App';
+
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(
