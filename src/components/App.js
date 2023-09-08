4568aa21acbada0b49145bad80372d27bec74947
Michael Carroll <mcarroll138@gmail.com>
2023-09-08 09:44:00 -0700

    update CoffeeList and Coffee Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
 import React from "react";
 import Header from "./Header"; 
+import CoffeeList from "./CoffeeList";
 
 
 function App(){
-        return (
+    
+    return (
         <React.Fragment>
             <Header />
+            <CoffeeList />
         </React.Fragment>
     );
 }
 
 export default App;
+
+
+
+// const coffeeName = "Colombia Viviana Realpe";
+// const coffeeRoast = "light";
+// const coffeeFlavor = "Peach, Grapefruit, Rose";
+// const coffeeFarm = "Los Nogales";
+// const coffeeOrigin = "Colombia";
+// const coffeeMethod = "Fully Washed & Dried on Raised Beds";
+// const coffeePrice = "$16.00";
+// const qty = 130;
+// <h3>Availible Beans:</h3>
+// <hr/>
+// <h3>Name: {coffeeName}</h3>
+// <h3>Roast Level: {coffeeRoast}</h3>
+// <h3>Flavor Notes: {coffeeFlavor}</h3>
+// <h3>Farm: {coffeeFarm}</h3>
+// <h3>Origin: {coffeeOrigin}</h3>
+// <h3>Processing Method: {coffeeMethod}</h3>
+// <h3>Price: {coffeePrice}</h3>
+// <h3>Quantity: {qty}</h3>
---
+++ src/components/Coffee.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Coffee(){
+    const coffeeName = "Colombia Viviana Realpe";
+    const coffeeRoast = "light";
+    const coffeeFlavor = "Peach, Grapefruit, Rose";
+    const coffeeFarm = "Los Nogales";
+    const coffeeOrigin = "Colombia";
+    const coffeeMethod = "Fully Washed & Dried on Raised Beds";
+    const coffeePrice = "$16.00";
+    const qty = 130;
+    return (
+        <React.Fragment>
+            <h3>Availible Beans:</h3>
+            <hr/>
+            <h3>Name: {coffeeName}</h3>
+            <h3>Roast Level: {coffeeRoast}</h3>
+            <h3>Flavor Notes: {coffeeFlavor}</h3>
+            <h3>Farm: {coffeeFarm}</h3>
+            <h3>Origin: {coffeeOrigin}</h3>
+            <h3>Processing Method: {coffeeMethod}</h3>
+            <h3>Price: {coffeePrice}</h3>
+            <h3>Quantity: {qty}</h3>
+            <hr/>
+        </React.Fragment>
+    );
+}
+
+export default Coffee;
